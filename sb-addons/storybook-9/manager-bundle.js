20acb10aaf78dec782fdc6814db88c329d29aa46
marknoya-eventful <marknoya@eventful.ph>
2023-06-02 01:16:16 +0800

    Updates

--- sb-addons/storybook-9/manager-bundle.js
+++ sb-addons/storybook-9/manager-bundle.js
@@ -1,4 +1,4 @@
 try{
-var O=__STORYBOOKADDONS__,{addons:o,types:c,mockChannel:l}=__STORYBOOKADDONS__;var g=__STORYBOOKTHEMING__,{CacheProvider:f,ClassNames:k,Global:y,ThemeProvider:u,background:x,color:C,convert:D,create:N,createCache:R,createGlobal:b,createReset:B,css:G,darken:K,ensure:Y,ignoreSsrWarning:v,isPropValid:P,jsx:A,keyframes:E,lighten:H,styled:I,themes:a,typography:M,useTheme:j,withTheme:w}=__STORYBOOKTHEMING__;o.setConfig({theme:{...a.dark,brandTitle:"Stark Design System"}});
+var O=__STORYBOOKADDONS__,{addons:o,types:c,mockChannel:l}=__STORYBOOKADDONS__;var T=__STORYBOOKTHEMING__,{CacheProvider:k,ClassNames:f,Global:y,ThemeProvider:b,background:u,color:x,convert:C,create:D,createCache:N,createGlobal:R,createReset:B,css:G,darken:K,ensure:Y,ignoreSsrWarning:v,isPropValid:I,jsx:P,keyframes:A,lighten:E,styled:H,themes:a,typography:M,useTheme:j,withTheme:w}=__STORYBOOKTHEMING__;o.setConfig({theme:{...a.dark,brandTitle:"Stark Design System",brandImage:"./Stark Logo.png"}});
 }catch(e){ console.error("[Storybook] One of your manager-entries failed: " + import.meta.url, e); }
 //# sourceMappingURL=manager-bundle.js.map
